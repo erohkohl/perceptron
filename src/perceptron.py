5c3d66c85c3776bca079faa0ce1a246e5a81fd9e
erik_rohkohl <erikrohkohl@googlemail.com>
2017-07-25 20:26:44 +0200

    implement the perceptron algorithm with testdriven approach

--- src/perceptron.py
+++ src/perceptron.py
@@ -1,2 +1,61 @@
-def test_method():
-    return 2
+"""
+This class implements a linear classifier with the perceptron algorithm for a initial weight, train and target vector.
+"""
+
+
+class Perceptron():
+    weights = []
+
+    def __init__(self, weight, rate, train_data, target_values):
+        self.weights.append(weight)
+        self.rate = rate
+        self.train_data = train_data
+        self.target_values = target_values
+
+    # First calculate all feature vectors, which are not correct classified with the current weight. Afterwards apply
+    # the perceptron learning rule to them.
+    def train(self):
+        weight_new = [0.0, 0.0, 0.0]
+        weight_old = self.weights[len(self.weights) - 1]
+        (is_lin_sep, y_w) = self.is_linear_separation(weight_old)
+
+        while is_lin_sep == False:
+            sum = [0.0, 0.0, 0.0]
+            for i in y_w:
+                sum[0] = sum[0] - 1
+                sum[1] = sum[1] - self.target_values[i] * self.train_data[0][i]
+                sum[2] = sum[2] - self.target_values[i] * self.train_data[1][i]
+
+            for i in range(0, 3, 1):
+                weight_new[i] = weight_old[i] - self.rate * sum[i]
+
+            self.weights.append(weight_new)
+            weight_old = self.weights[len(self.weights) - 1]
+            (is_lin_sep, y_w) = self.is_linear_separation(weight_old)
+
+        return self.weights
+
+    # Method determines whether the current weight classifies a feature vector as member of class 1 or -1.
+    def classify(self, weight, x_vec):
+        value = weight[0]
+        for i in range(1, 3, 1):
+            value = value + weight[i] * x_vec[i - 1]
+        if value > 0:
+            return 1
+        return -1
+
+    # Method checks if all of the train data is separated into two disjoint classes by the current weight vector.
+    def is_linear_separation(self, weight):
+
+        # Stores all not correct classified feature vectors
+        y_w = []
+        for i in range(0, len(self.train_data[0]), 1):
+            x_1 = self.train_data[0][i]
+            x_2 = self.train_data[1][i]
+            if self.classify(weight, [x_1, x_2]) != self.target_values[i]:
+                y_w.append(i)
+
+        if y_w == []:
+            return (True, y_w)
+        else:
+            return (False, y_w)
--- test/perceptron.py
+++ test/perceptron.py
@@ -2,7 +2,49 @@ import unittest
 
 import src.perceptron
 
-class TestPerceptorn(unittest.TestCase):
 
-    def first_test_case(self):
-        self.assertEqual(src.perceptron.test_method(), 2)
+class TestPerceptorn(unittest.TestCase):
+    def test_init(self):
+        train_data = [[1.0, 2.0, 4.0, 3.0], [2.0, 3.0, 3.0, 5.0]]
+        target_values = [1, 1, -1, -1]
+        weight = [4.0, -1.5, -1.0]
+        rate = 0.1;
+
+        perceptron = src.perceptron.Perceptron(weight, rate, train_data, target_values)
+        self.assertEqual(perceptron.weights.pop(), weight)
+        self.assertEqual(perceptron.rate, rate)
+        self.assertEqual(perceptron.train_data, train_data)
+        self.assertEqual(perceptron.target_values, target_values)
+
+    def test_train(self):
+        train_data = [[1.0, 2.0, 4.0, 3.0], [2.0, 3.0, 3.0, 5.0]]
+        target_values = [1, 1, -1, -1]
+        weight = [4.0, -1.5, -1.0]
+        rate = 0.1;
+
+        perceptron = src.perceptron.Perceptron(weight, rate, train_data, target_values)
+
+        result = perceptron.train().pop();
+        for i in range(0, 3, 1):
+            result[i] = round(result[i], 1)
+        self.assertEqual(result, [4.2, -1.1, -0.4])
+
+    def test_classify(self):
+        weight = [4.1, -1.3, -0.7]
+        train_data = [[1.0, 2.0, 4.0, 3.0], [2.0, 3.0, 3.0, 5.0]]
+        target_values = [1, 1, -1, -1]
+        rate = 0.1;
+
+        perceptron = src.perceptron.Perceptron(weight, rate, train_data, target_values)
+        result = perceptron.classify(weight, [3.0, 4.0])
+        self.assertEqual(result, -1)
+
+    def test_linear_separation(self):
+        weight = [4.2, -1.1, -0.4]
+        train_data = [[1.0, 2.0, 4.0, 3.0], [2.0, 3.0, 3.0, 5.0]]
+        target_values = [1, 1, -1, -1]
+        rate = 0.1;
+
+        perceptron = src.perceptron.Perceptron(weight, rate, train_data, target_values)
+        result = perceptron.is_linear_separation(weight)
+        self.assertTrue(result[0])
